2cbf8b2616eb2b44bb78268eecf5612813d2ba25
Joao-Henrique16 <joao60121986@aluno.ba.senac.br>
2025-12-12 10:56:32 -0300

    Update helloword.java

--- helloword.java
+++ helloword.java
@@ -1,6 +1,6 @@
 public class helloword {
    public static void main(String[] args) {
-        // O comando System.out.println() exibe o texto na console
         System.out.println("Hello, World!");
     }
 }  
+
